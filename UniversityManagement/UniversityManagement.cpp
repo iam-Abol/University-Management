7df6c1f8bc878dd0b5c5dc40c23d027c129ae8ab
iam-Abol <mh.z77w@gmail.com>
2020-11-07 15:20:26 +0330

    getters and setters finished

--- UniversityManagement/UniversityManagement.cpp
+++ UniversityManagement/UniversityManagement.cpp
@@ -4,17 +4,10 @@
 #include "Student.h"
 #include <vector>
 #include "FileManagement.h"
+#include "Lesson.h"
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-
-
-	std::vector <Student> students;
-	
-	
-	
-	std::cin.get();
-	std::cin.get();
 	return 0;
 }
 
